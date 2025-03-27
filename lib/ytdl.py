0c516926078835cfdb413e507760e67d6b35524c
Xnodes Development <xnodesdev@gmail.com>
2025-03-27 23:18:06 +0530

    ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ10.2 🧬

--- lib/ytdl.py
+++ lib/ytdl.py
@@ -1,11 +1,12 @@
 import yt_dlp
 import json
 import sys
+import os
 
-def download_media(url, media_type, quality):
+def download_media(url, media_type, quality, store_dir):
     ydl_opts = {
         'format': f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}]' if media_type == 'video' else f'bestaudio[abr<=128]/best',
-        'outtmpl': '%(id)s.%(ext)s',
+        'outtmpl': os.path.join(store_dir, '%(id)s.%(ext)s'),
         'quiet': True,
         'no_warnings': True,
         'extract_flat': False,
@@ -30,9 +31,10 @@ def download_media(url, media_type, quality):
         return {'success': False, 'error': str(e)}
 
 if __name__ == "__main__":
-    try:
-        args = json.loads(sys.argv[1])
-        result = download_media(args['url'], args['media_type'], args['quality'])
-        print(json.dumps(result))
-    except Exception as e:
-        print(json.dumps({'success': False, 'error': str(e)}))
+    url = sys.argv[1]
+    media_type = sys.argv[2]
+    quality = sys.argv[3]
+    store_dir = sys.argv[4]
+    
+    result = download_media(url, media_type, quality, store_dir)
+    print(json.dumps(result))
