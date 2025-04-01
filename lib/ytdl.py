d84cd3e3a4b622bee8cc73623063d51bc31937d4
Xnodes Development <xnodesdev@gmail.com>
2025-04-01 23:19:03 +0530

    ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ10.2 🧬

--- lib/ytdl.py
+++ lib/ytdl.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
-K='error'
-F='success'
-C=False
+L='error'
+G='success'
+F=False
 B=True
-import yt_dlp as P,json,sys as D,os as A
-from contextlib import redirect_stdout as Q,redirect_stderr as R
-from io import StringIO as I
-J=A.path.join(A.path.dirname(__file__),'cookies.txt')
+import yt_dlp as R,json,sys as C,os as A
+from contextlib import redirect_stdout as S,redirect_stderr as T
+from io import StringIO as J
+K=A.path.join(A.path.dirname(__file__),'cookies.txt')
 E=A.path.join(A.path.dirname(__file__),'store')
 def H(url):
-	O='duration';N='title';D={'outtmpl':A.path.join(E,'%(id)s.%(ext)s'),'quiet':B,'no_warnings':B,'nocheckcertificate':B,'retries':2,'progress':C,'extract_flat':C,'socket_timeout':30,'nopart':B,'http_chunk_size':10485760,'format':'bestvideo[height<=360]','merge_output_format':'mp4'}
-	if A.path.exists(J):D['cookiefile']=J
-	D['extractor_args']={'youtube':{'skip':['dash','hls','translated_subs'],'player_skip':['configs','webpage']}};L=I();M=I()
+	Q='duration';I='title';M={'outtmpl':A.path.join(E,'%(id)s.%(ext)s'),'quiet':B,'no_warnings':B,'nocheckcertificate':B,'socket_timeout':10,'retries':2,'progress_hooks':[lambda d:None],'format':'bestvideo[height<=360]+bestaudio/best[height<=360]/best','postprocessors':[{'key':'FFmpegVideoRemuxer','preferedformat':'mp4'}]}
+	if A.path.exists(K):M['cookiefile']=K
+	N=J();O=J()
 	try:
-		with Q(L),R(M):
-			with P.YoutubeDL(D)as G:G.params['check_formats']=C;H=G.extract_info(url,download=B);S=G.prepare_filename(H);return{F:B,'filename':A.path.abspath(S),N:H.get(N,'Unknown'),O:H.get(O,0)}
-	except Exception as T:return{F:C,K:f"Error: {str(T)}"}
-	finally:L.close();M.close()
+		with S(N),T(O):
+			with R.YoutubeDL(M)as H:
+				C=H.extract_info(url,download=F)
+				if not C or I not in C:raise ValueError('Failed to extract metadata')
+				H.download([url]);D=H.prepare_filename(C);P='.mp4'
+				if not D.endswith(P):U,W=A.path.splitext(D);D=f"{U}{P}"
+				return{G:B,'filename':A.path.abspath(D),I:C.get(I,'Unknown'),Q:C.get(Q,0)}
+	except Exception as V:return{G:F,L:f"Error: {str(V)}"}
+	finally:N.close();O.close()
 if __name__=='__main__':
-	if len(D.argv)!=2:G={F:C,K:'Usage: python ytdl.py <url>'}
+	if len(C.argv)!=2:D={G:F,L:'Usage: python ytdl.py <url>'}
 	else:
-		L=D.argv[1]
+		I=C.argv[1]
 		if not A.path.exists(E):A.makedirs(E,exist_ok=B)
-		G=H(L)
-	D.stdout.write(json.dumps(G));D.stdout.flush()
+		D=H(I)
+	C.stdout.write(json.dumps(D));C.stdout.flush()
